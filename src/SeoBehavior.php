7469ea8f9fd8f949fa4c33fbfbebf27b686bb20c
RodrigoDornelles <rodrigo@dornelles.me>
2021-06-01 15:55:29 -0300

    Create SeoBehavior.php

---
+++ src/SeoBehavior.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace dynamikaweb\seo;
+
+class SeoBehavior extends \yii\behaviors\AttributeBehavior
+{   
+    public $metatags = [];
+    public $attribute = 'seo';
+
+    /**
+     * @inheritdoc
+     */
+    public function canGetProperty($name, $checkVars = true)
+    {
+        if ($name !== $this->attribute) {
+            return parent::canGetProperty($name, $checkVars);
+        }
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function __get($name)
+    {
+        if ($name !== $this->attribute) {
+            return null;
+        }
+
+        $metatags = [];
+
+        foreach($this->metatags as $tag => $attribute) {
+            if ($this->owner->canGetProperty($attribute)) {
+                $metatags[$tag] = $this->owner->{$attribute};
+            }
+        }
+
+        return $metatags;
+    }
+}
